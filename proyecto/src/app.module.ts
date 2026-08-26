994eda8f0e1dc4af85a285bdde2019eab4d37367
leop <yukaleo43@gmail.com>
2026-08-26 20:29:03 -0300

    arreglo para el despliegue

--- proyecto/src/app.module.ts
+++ proyecto/src/app.module.ts
@@ -48,7 +48,12 @@ import { BusquedasModule } from './modules/gestion-documentos/busquedas/busqueda
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       // entities,
       synchronize: false,  
-      ssl: process.env.DB_SSL === 'true',
+      ssl:
+        process.env.DB_SSL === 'true'
+          ? {
+              rejectUnauthorized: false, // Permite conexiones seguras con certificados de servicios cloud
+            }
+          : undefined,
     }),
 
     MarcaModule,
